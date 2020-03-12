4e628229c0bf6c84a6c77ee444ead2b41b985ac3
chakapega <chakapega@gmail.com>
2020-03-13 02:57:27 +0300

    feat: Added login request and its processing; added login form, buttons for future get requests

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,62 @@
 const signupForm = document.querySelector('.signup-form');
+const loginForm = document.querySelector('.login-form');
 
-signupForm.addEventListener('submit', event => {
-  event.preventDefault();
+signupForm.addEventListener('submit', async event => {
+  try {
+    event.preventDefault();
 
-  const { value: email } = event.target.email;
-  const { value: password } = event.target.password;
-  console.log(email, password);
+    const { value: email } = event.target.email;
+    const { value: password } = event.target.password;
+    const response = await fetch('http://localhost:5000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+    const data = await response.json();
 
-  fetch('http://localhost:5000/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      email,
-      password
-    })
-  });
+    if (!response.ok) throw new Error(data.message);
+
+    const { message, userId, token } = data;
+
+    localStorage.setItem('userData', JSON.stringify({ userId, token }));
+    signupForm.reset();
+    alert(message);
+  } catch (error) {
+    alert(error.message || 'An error occured, please try again');
+  }
+});
+
+loginForm.addEventListener('submit', async event => {
+  try {
+    event.preventDefault();
+
+    const { value: email } = event.target.email;
+    const { value: password } = event.target.password;
+    const response = await fetch('http://localhost:5000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+    const data = await response.json();
+
+    if (!response.ok) throw new Error(data.message);
+
+    const { message, userId, token } = data;
+
+    localStorage.setItem('userData', JSON.stringify({ userId, token }));
+    loginForm.reset();
+    alert(message);
+  } catch (error) {
+    alert(error.message || 'An error occured, please try again');
+  }
 });
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 const mongoURI =
   'mongodb+srv://chakapega:chakapegaomertextt@cluster0-3yi6p.azure.mongodb.net/test?retryWrites=true&w=majority';
 const PORT = 5000;
+const jwtSecret = 'omertex-tt';
 
 server.use(cors());
 server.use(express.json({ extended: true }));
@@ -13,16 +15,43 @@ server.use(express.json({ extended: true }));
 server.post('/signup', async (request, response) => {
   try {
     const { email, password } = request.body;
-    const potentialUser = await User.findOne({ email });
+    const potentialUser = await User.findOne({ userId: email });
 
     if (potentialUser) {
-      return response.status(400).json({ message: 'This user already exists' });
+      return response.status(400).json({ message: 'This account already exists' });
     }
 
-    const user = new User({ email, password, userIdType: 'email' });
+    const user = new User({ userId: email, password, userIdType: 'email' });
 
     await user.save();
-    response.status(201).json({ message: 'User created' });
+    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '5m' });
+
+    response
+      .status(201)
+      .json({ message: 'The account was created successfully and you are logged in', token, userId: user.id });
+  } catch (error) {
+    response.status(500).json({
+      message: error.message || 'An error occured, please try again'
+    });
+  }
+});
+
+server.post('/login', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+    const user = await User.findOne({ userId: email });
+
+    if (!user) {
+      return response.status(400).json({ message: 'Account is not found' });
+    }
+
+    if (user.password !== password) {
+      return response.status(400).json({ message: 'Wrong password' });
+    }
+
+    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: '5m' });
+
+    response.json({ message: 'You have successfully logged in', token, userId: user.id });
   } catch (error) {
     response.status(500).json({
       message: error.message || 'An error occured, please try again'
